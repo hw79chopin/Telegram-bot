bdd42e79d3b492c67cd38a8903f0db36ba275ba4
HyunWoo Jung <chopin_liszt@naver.com>
2020-08-24 21:10:09 +0900

    Telegram bot ver3.0 uploaded

--- bot/bot.py
+++ bot/bot.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*- 
+# 누나 ID: 1238642275
+# 나의 ID: 1179537484
 from crawler import Crawler
 import telegram
+import json
 import time
 
 # bot 세팅
-TOKEN = 'telegram_token'
+TOKEN = 'Token 입력하시고'
 bot = telegram.Bot(token=TOKEN)
 
 # 크롤러 세팅
-crawler = Crawler("chrome-driver-directory")
+crawler = Crawler("chromedriver 주소 입력하시고")
 count = 0 
 
 # 알림봇 일하기 시작!
@@ -17,23 +20,31 @@ while True:
     count += 1
     now = time.strftime('%H%M%S')
     print("*"*15,"{}시 {}분에 {}차 시작~".format(now[:2], now[2:4], count), "*"*15)
+
+    # 과거 데이터 불러오기
+    with open('info_crawled.json','r') as outfile:
+        past_data = json.load(outfile)
+
+    new_data = crawler.crawl_data()
     
-    result = crawler.crawl_data()
-    if count == 1:
-        previous_result = result
-    
-    new_info = list(set(result.keys() - set(previous_result.keys())))
+    new_info = list(set(new_data.keys() - set(past_data.keys())))
     print("새로운 정보: {}건".format(len(new_info)))
+
     if new_info == []:
         pass
     elif new_info != []:
-        for new in new_info:
-            region = result[new]['근무지역']
-            title = result[new]['제목']
-            URL = result[new]['URL']
-            bot.send_message(chat_id=telegram_id1, text="새로운 공고가 나왔어요!!!\n* 지역: {}\n* 제목: <{}>\n관심있으면 여길 클릭하숑!\n{}".format(region, title, URL))
-            bot.send_message(chat_id=telegram_id2, text="새로운 공고가 나왔어요!!!\n* 지역: {}\n* 제목: <{}>\n관심있으면 여길 클릭하숑!\n{}".format(region, title, URL))
+        for new in sorted(new_info):
+            region = new_data[new]['근무지역']
+            title = new_data[new]['제목']
+            info = new_data[new]['세부사항']
+            URL = new_data[new]['URL']
 
-    previous_result = result
+            bot.send_message(chat_id="id입력하시고", text="새로운 공고가 나왔어요!!!\n\n* 제목: {}\n* 지역: <{}>\n*세부사항: {} \n\n\n 관심있으면 여길 클릭하숑!\n{}".format(title, region, info, URL))
+
+    new_data.update(past_data)
+
+    with open('info_crawled.json', 'w') as outfile:
+        json.dump(new_data, outfile, indent=4)
+
     print("끝!")
-    time.sleep(1200)
+    time.sleep(600)
