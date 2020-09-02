8b0b64e819fa271a621680da259a50fb620c89e0
HyunWoo Jung <chopin_liszt@naver.com>
2020-09-02 23:24:19 +0900

    Telegram bot ver4.0 uploaded. combined with quote reminder

--- bot/bot.py
+++ bot/bot.py
@@ -1,50 +1,36 @@
 # -*- coding: utf-8 -*- 
-# 누나 ID: 1238642275
-# 나의 ID: 1179537484
-from crawler import Crawler
-import telegram
 import json
 import time
-
-# bot 세팅
-TOKEN = 'Token 입력하시고'
-bot = telegram.Bot(token=TOKEN)
+from crawler import Crawler, TeleBot
+from messenger import Messenger
+from datetime import date
 
 # 크롤러 세팅
-crawler = Crawler("chromedriver 주소 입력하시고")
-count = 0 
+crawler = Crawler()
+telebot = TeleBot()
 
 # 알림봇 일하기 시작!
+count = 0
 while True:
-    # 시간 및 횟수 측정!
     count += 1
-    now = time.strftime('%H%M%S')
-    print("*"*15,"{}시 {}분에 {}차 시작~".format(now[:2], now[2:4], count), "*"*15)
+    # 시간 및 횟수 측정!
+    if count == 1:
+        previous_day = 1
+    today, now = date.today(), time.strftime('%H%M%S')
+    print("*"*15,"{} {}시 {}분에 시작~".format(today, now[:2], now[2:4]), "*"*15)
 
-    # 과거 데이터 불러오기
-    with open('info_crawled.json','r') as outfile:
-        past_data = json.load(outfile)
+    # 누나를 위한 텔레그램 봇 
+    past_data = crawler.load_past_data('info_crawled.json')
+    new_data = crawler.crawl_data("chromedriver")
 
-    new_data = crawler.crawl_data()
+    telebot.send_message(past_data, new_data)
+    telebot.update_and_save_data(past_data, new_data, 'info_crawled.json')
+
+    # 나를 위한 봇
+    if today != previous_day:
+        model = Messenger()
+        model.send_quote()
     
-    new_info = list(set(new_data.keys() - set(past_data.keys())))
-    print("새로운 정보: {}건".format(len(new_info)))
-
-    if new_info == []:
-        pass
-    elif new_info != []:
-        for new in sorted(new_info):
-            region = new_data[new]['근무지역']
-            title = new_data[new]['제목']
-            info = new_data[new]['세부사항']
-            URL = new_data[new]['URL']
-
-            bot.send_message(chat_id="id입력하시고", text="새로운 공고가 나왔어요!!!\n\n* 제목: {}\n* 지역: <{}>\n*세부사항: {} \n\n\n 관심있으면 여길 클릭하숑!\n{}".format(title, region, info, URL))
-
-    new_data.update(past_data)
-
-    with open('info_crawled.json', 'w') as outfile:
-        json.dump(new_data, outfile, indent=4)
-
     print("끝!")
+    previous_day = today
     time.sleep(600)
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*- 
+import json
 import time
+import telegram
 import numpy as np
 import pandas as pd
 import urllib.request
+from datetime import date
 from tqdm.notebook import tqdm
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,12 +13,12 @@ from selenium.webdriver import ActionChains
 
 
 class Crawler:
-    def __init__(self, webdriver_dir):
-        self.webdriver_dir = webdriver_dir
+    def __init__(self):
+        self.name = "나는 착한 로봇이당!"
 
-
-    def crawl_data(self):
-        driver = webdriver.Chrome(r"{}".format(self.webdriver_dir))
+    def crawl_data(self, webdriver_dir):
+        webdriver_dir = webdriver_dir
+        driver = webdriver.Chrome(r"{}".format(webdriver_dir))
 
         driver.get('http://recruit.dailypharm.com/Search.php?mode=offer&order=reg&optionAreaVal%5B%5D=97&optionAreaVal%5B%5D=100&optionAreaVal%5B%5D=104&optionAreaVal%5B%5D=106&optionAreaVal%5B%5D=107&optionAreaVal%5B%5D=109&optionAreaVal%5B%5D=110&optionAreaVal%5B%5D=111&optionJobVal%5B%5D=17&optionJobVal%5B%5D=12&optionJobVal%5B%5D=13&keyword=')
 
@@ -68,5 +71,36 @@ class Crawler:
 
         driver.quit()
         data = dict_info.copy()
-
         return data
+
+    def load_past_data(self, url):
+        with open(url,'r') as f:
+            data = json.load(f)
+        return data
+
+class TeleBot:
+    def __init__(self):
+        self.name = "나는 착한 봇이다."
+
+    def send_message(self, past_data, new_data):
+        TOKEN = 'token'
+        bot = telegram.Bot(token=TOKEN)
+        new_info = list(set(new_data.keys() - set(past_data.keys())))
+        print("새로운 정보: {}건".format(len(new_info)))
+
+        if new_info == []:
+            pass
+
+        elif new_info != []:
+            for new in sorted(new_info):
+                region = new_data[new]['근무지역']
+                title = new_data[new]['제목']
+                info = new_data[new]['세부사항']
+                URL = new_data[new]['URL']
+
+                bot.send_message(chat_id=id, text="새로운 공고가 나왔어요!!!\n\n* 제목: {}\n* 지역: <{}>\n* 세부사항: {} \n\n\n 관심있으면 여길 클릭하숑!\n{}".format(title, region, info, URL))
+
+    def update_and_save_data(self, past_data, new_data, dir):
+        new_data.update(past_data)
+        with open(dir, 'w') as outfile:
+            json.dump(new_data, outfile, indent=4)
