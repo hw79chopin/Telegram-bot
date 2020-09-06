a6e0307c212eb2a924f16730dd5d7209bab38400
HyunWoo Jung <chopin_liszt@naver.com>
2020-09-06 22:35:36 +0900

    Secret info deleted

--- crawling bot ver2.0/bot.py
+++ crawling bot ver2.0/bot.py
@@ -16,12 +16,12 @@ while True:
     print("*"*15,"{} {}시 {}분에 시작~".format(today, now[:2], now[2:4]), "*"*15)
 
     # Crawl and Send telegram message
-    past_data = crawler.load_past_data('/Users/junghyunwoo/Downloads/crawled_data.json')
+    past_data = crawler.load_past_data('crawled_data.json')
     new_data = crawler.crawl_data()
     teleBot.send_message(past_data, new_data)
 
     # Save new_info
-    teleBot.update_and_save_data(past_data, new_data, '/Users/junghyunwoo/Downloads/crawled_data.json')
+    teleBot.update_and_save_data(past_data, new_data, 'crawled_data.json')
 
     print("Finished")
     time.sleep(600)
--- crawling bot ver2.0/teleBot.py
+++ crawling bot ver2.0/teleBot.py
@@ -10,7 +10,7 @@ class TeleBot:
         self.name = "I am a good robot"
 
     def send_message(self, past_data, new_data):
-        TOKEN = '1257448855:AAGU5nTrmFI--06ZRUTBfn9Wqq_oZ4_BLD8'
+        TOKEN = 'Token'
         bot = telegram.Bot(token=TOKEN)
         new_info = list(set(new_data.keys() - set(past_data.keys())))
         print("새로운 정보: {}건".format(len(new_info)))
@@ -25,10 +25,7 @@ class TeleBot:
                 info = new_data[new]['details']
                 URL = new_data[new]['URL']
 
-                # 누나 ID: 1238642275
-                # 나의 ID: 1179537484
-                bot.send_message(chat_id=1179537484, text="새로운 공고가 나왔어요!!!\n\n* 제목: {}\n* 지역: {}\n* 세부사항: {} \n\n\n 관심있으면 여길 클릭하숑!\n{}".format(title, region, info, URL))
-                # bot.send_message(chat_id=1238642275, text="새로운 공고가 나왔어요!!!\n\n* 제목: {}\n* 지역: <{}>\n* 세부사항: {} \n\n\n 관심있으면 여길 클릭하숑!\n{}".format(title, region, info, URL))
+                bot.send_message(chat_id=telegramId, text="새로운 공고가 나왔어요!!!\n\n* 제목: {}\n* 지역: {}\n* 세부사항: {} \n\n\n 관심있으면 여길 클릭하숑!\n{}".format(title, region, info, URL))
 
     def update_and_save_data(self, past_data, new_data, dir):
         new_data.update(past_data)
